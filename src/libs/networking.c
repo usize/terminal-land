280e5f263925d5070b1f041560d1b7db98e90960
usize <mcfoster1228@gmail.com>
2020-09-29 23:51:10 -0400

    Add networking caveat

--- src/libs/networking.c
+++ src/libs/networking.c
@@ -1,5 +1,8 @@
 #include "networking.h"
 
+// This implementation is terrible because it doesn't account for
+// differences between architectures or operating systems. These
+// include: endianness, structure packing, and type sizes.
 Message_t Networking_new_message(message_payload_type type, 
                                    void* payload,
                                    size_t payload_size) {
